d84f1efc7a0a92dbfa4bc0ff5f39ece47c877ec9
Squid233 <60126026+squid233@users.noreply.github.com>
2020-09-06 14:52:22 +0800

    Update Main.java

--- src/io/github/scopetech/crazypointer/Main.java
+++ src/io/github/scopetech/crazypointer/Main.java
@@ -1,47 +1,72 @@
 package io.github.scopetech.crazypointer;
 
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Scanner;
 
+/**
+ * @author squid233
+ */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String version = "1.1";
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ignored) { }
+        String version = "1.2";
         System.out.println("CrazyPointer VM " + version + " is ready. Reading file...");
         Scanner sc = new Scanner(System.in);
-        boolean isNotExit = true;
-        Reader reader;
+        Reader r = null;
         try {
-            reader = new FileReader(args[0]);
-        } catch (FileNotFoundException | ArrayIndexOutOfBoundsException e) {
+            r = new FileReader(args[0]);
+        } catch (ArrayIndexOutOfBoundsException | FileNotFoundException e) {
             System.out.println("File in parameter is not found. Please enter the file:");
             try {
-                reader = new FileReader(sc.nextLine());
-            } catch (FileNotFoundException | ArrayIndexOutOfBoundsException e1) {
-                System.out.println("File in parameter is not found. Please enter the file:");
-                reader = new FileReader(sc.nextLine());
+                r = new FileReader(sc.nextLine());
+            } catch (FileNotFoundException ee) {
+                JOptionPane.showMessageDialog(null,
+                        "Error: Could not create the Java Virtual Machine.\nError: A fatal exception has occurred. Program will exit.",
+                        "Java Virtual Machine Launcher", JOptionPane.ERROR_MESSAGE);
+                System.exit(-1);
             }
         }
-        while (isNotExit) {
-            char pointer = 0;
-            int a;
-            while ((a = reader.read()) != -1) {
-                if ((char) a == '>') pointer++;
-                else if ((char) a == '<') pointer--;
-                else if ((char) a == '.') System.out.print(pointer);
+        while (true) {
+            char c = 0;
+            int ptr = 0;
+            while (true) {
+                try {
+                    if ((ptr = r.read()) == -1) {
+                        break;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                char ptrC = (char) ptr;
+                if (ptrC == '>') {
+                    c++;
+                } else if (ptrC == '<') {
+                    c--;
+                } else if (ptrC == '.') {
+                    System.out.print(c);
+                }
             }
             System.out.println("\nThe file has been read successful! Do you want to exit this program?(y/n)");
-            if (sc.nextLine().equalsIgnoreCase("y")) {
-                isNotExit = false;
+            if ("y".equalsIgnoreCase(sc.nextLine())) {
+                System.out.println("Thanks for use!");
+                System.exit(0);
             } else {
                 System.out.println("Please enter the file:");
-                reader = new FileReader(sc.nextLine());
+                try {
+                    r = new FileReader(sc.nextLine());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
+            System.out.println("Thanks for use!");
         }
-        System.out.println("Thanks for use!");
-        reader.close();
-        System.exit(0);
     }
 }
